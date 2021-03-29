d2ec5b1fcf80cb8c53806be7a58dcece307d793b
DESKTOP-MCSM6G7\Mert <mert.akbulut.bd@gmail.com>
2021-03-29 12:13:59 +0300

    +Created FirstLessonScript

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField]
+        int tamSayi = 1;
+        [SerializeField]
+        int tamSayi2 = 100;
+        float ondalikSayi = 0.25f;
+        float ondalikSayi2 = 1.25f;
+        bool dogruYanlis = true;
+        bool dogruYanlis2 = false;
+
+
+
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
