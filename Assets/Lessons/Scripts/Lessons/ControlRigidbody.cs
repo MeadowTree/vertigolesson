dffa56446e01a428ac02bf9f2cd5d6b978112cd1
Mert <glacier_mage_mrt@hotmail.com>
2021-04-12 14:56:07 +0300

    no message

---
+++ Assets/Lessons/Scripts/Lessons/ControlRigidbody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Lessons
+{
+    public class ControlRigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private ControlRigidbody_settings _Settings;
+
+        // Update is called once per frame
+        void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(_Settings.JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
+
---
+++ Assets/Lessons/Scripts/Lessons/RotateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Lessons
+{ 
+    public class RotateObject : MonoBehaviour
+    {
+       
+       [SerializeField] private RotateSettings _rotateSettings;
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _rotateSettings.Speed * Time.deltaTime);
+            ///100 frame per second * 0.1  //////// 50 frame per second 50 * .2
+        }
+    }
+
+}
