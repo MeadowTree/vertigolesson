b259f60195c356790256eddfdc30ddb14d043e6e
DESKTOP-MCSM6G7\Mert <mert.akbulut.bd@gmail.com>
2021-04-22 00:42:21 +0300

    +refactor inputsystem

--- Assets/TopDownShooter/Scripts/Input/InputData.cs
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -10,6 +10,63 @@ namespace TopDownShooter.PlayerInput
     {
         public float Horizontal;
         public float Vertical;
+
+        [Header("Axis Base Control")]
+        [SerializeField] private bool _axisActive;
+        [SerializeField] private string AxisNameHorizontal;
+        [SerializeField] private string AxisNameVertical;
+
+
+        [Header("Key Base Control")]
+        [SerializeField] private bool _keyBaseHorizontalActive;
+        [SerializeField] private KeyCode PozitiveHorizontalKeyCode;
+        [SerializeField] private KeyCode NegativeHorizontalKeyCode;
+        [SerializeField] private bool _keyBaseVerticalActive;
+        [SerializeField] private KeyCode PozitiveVerticalKeyCode;
+        [SerializeField] private KeyCode NegativeVerticalKeyCode;
+        [SerializeField] private float _increaseAmont = 1;
+
+
+        public void ProcessInput()
+        {
+            if (_axisActive)
+            {
+                Horizontal = Input.GetAxis(AxisNameHorizontal);
+                Vertical = Input.GetAxis(AxisNameVertical);
+            }
+            else
+            {
+                if (_keyBaseHorizontalActive)
+                {
+                    KeyBaseAxisControl(ref Horizontal, PozitiveHorizontalKeyCode, NegativeHorizontalKeyCode);
+                }
+                if (_keyBaseVerticalActive)
+                {
+                    KeyBaseAxisControl(ref Vertical, PozitiveVerticalKeyCode, NegativeVerticalKeyCode);
+                }
+            }
+        }
+
+        private void KeyBaseAxisControl(ref float value,KeyCode positive, KeyCode negative)
+        {
+            bool pozitiveActive = Input.GetKey(positive);
+            bool NegativeActive = Input.GetKey(negative);
+            if (pozitiveActive)
+            {
+                value += _increaseAmont;
+            }
+            else if (NegativeActive)
+            {
+                value -= _increaseAmont;
+            }
+            else
+            {
+                value = 0;
+            }
+
+            value = Mathf.Clamp(value, -1, 1);
+        }
+
     }
 }
 
