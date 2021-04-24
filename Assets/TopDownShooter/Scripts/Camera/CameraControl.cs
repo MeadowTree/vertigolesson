1b736b124d2096f395267e469b9a15a88b32c80d
DESKTOP-MCSM6G7\Mert <mert.akbulut.bd@gmail.com>
2021-04-24 16:53:34 +0300

    +Camera fix

--- Assets/TopDownShooter/Scripts/Camera/CameraControl.cs
+++ Assets/TopDownShooter/Scripts/Camera/CameraControl.cs
@@ -9,32 +9,25 @@ namespace TopDownShooter.Camera
     public class CameraControl : MonoBehaviour
     {
         [SerializeField] private CameraSettings _cameraSettings;
-        [SerializeField] private Transform _rotationtarget;
-        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _rotationTarget;
+        [SerializeField] private Transform _positionTarget;
         [SerializeField] private Transform _cameraTransform;
-
-        public ShootingManager _shootingManager;
 
         private void Update()
         {
-            CameraRotationFollow();
             CameraMovementFollow();
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _shootingManager.Shoot(_cameraTransform.position, _cameraTransform.forward);
-            }
+            CameraRotationFollow();
         }
 
         private void CameraRotationFollow()
         {
-            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_rotationtarget.forward), Time.deltaTime * _cameraSettings.RotationLerpSpeed);
-
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_rotationTarget.forward), Time.deltaTime * _cameraSettings.RotationLerpSpeed);
         }
+
 
         private void CameraMovementFollow()
         {
-            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _targetTransform.position + _cameraSettings.PositionOffset, Time.deltaTime * _cameraSettings.PositionLerp);
-
+            _cameraTransform.localPosition = _cameraSettings.PositionOffset;
         }
 
     }
